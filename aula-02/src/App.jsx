d748f187940062977c9d28d79bc9dfd9c8bb0ff5
wesleysilvacocreare <123510367+wesleysilvacocreare@users.noreply.github.com>
2023-02-14 22:14:25 -0300

    first commit

---
+++ aula-02/src/App.jsx
@@ -0,0 +1,16 @@
+// const App = () => <h1>Meu primeiro componente</h1>;
+
+function App() {
+  return (
+    <>
+      {/* Fragmento */}
+      <h1>Meu primeiro componente</h1>
+      <p>Meu texto</p>
+      <ul>
+        <li>Item 1</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
